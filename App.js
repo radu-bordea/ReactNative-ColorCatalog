df3e1eccb2b125933ffa73844968bd20906dd7cd
radu-bordea <radu.bordea.dev@gmail.com>
2023-11-22 23:44:39 +0100

    finish demo project

--- App.js
+++ App.js
@@ -1,11 +1,30 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from "react";
+import { Text, View, StyleSheet, TouchableHighlight } from "react-native";
+
+function ColorButton({ backgroundColor, onPress = (f) => f }) {
+  return (
+    <TouchableHighlight
+      style={styles.button}
+      onPress={() => onPress(backgroundColor)}
+      underlayColor="orange"
+    >
+      <View style={styles.row}>
+        <View style={[styles.sample, { backgroundColor }]} />
+        <Text style={styles.buttonText}>{backgroundColor}</Text>
+      </View>
+    </TouchableHighlight>
+  );
+}
 
 export default function App() {
+  const [backgroundColor, setBackgroundColor] = useState("blue");
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={[styles.container, { backgroundColor }]}>
+      <ColorButton backgroundColor="red" onPress={setBackgroundColor} />
+      <ColorButton backgroundColor="green" onPress={setBackgroundColor} />
+      <ColorButton backgroundColor="blue" onPress={setBackgroundColor} />
+      <ColorButton backgroundColor="yellow" onPress={setBackgroundColor} />
+      <ColorButton backgroundColor="purple" onPress={setBackgroundColor} />
     </View>
   );
 }
@@ -13,8 +32,31 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  button: {
+    margin: 10,
+    padding: 10,
+    borderWidth: 2,
+    borderRadius: 10,
+    alignSelf: "stretch",
+    backgroundColor: "rgba(255, 255, 255, 0.8)",
+  },
+  buttonText: {
+    fontSize: 30,
+    textAlign: "center",
+  },
+  row: {
+    flexDirection: "row",
+    alignItems: "center",
+  },
+  sample: {
+    height: 20,
+    width: 20,
+    margin: 5,
+    borderRadius: 10,
+    backgroundColor: "white",
   },
 });
---
+++ Readme.md
@@ -0,0 +1,9 @@
+
+// commands
+- npx create-expo-app ColorCatalog
+- npm run android
+- npm run ios
+- npm run web
+- npm start
+- npx expo install react-native-web@~0.19.6 react-dom@18.2.0 @expo/webpack-config@^19.0.0
+
